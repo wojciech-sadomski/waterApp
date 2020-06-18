b5870745af824ac1067d9f2ab855b84982d6119e
Wojciech Sadomski <wsadomski@gmail.com>
2020-06-18 17:52:08 +0200

    working app, error in

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,42 @@ import "../scss/main.scss";
 // registerSW();
 
 /* place your code below */
+const btnAdd = document.querySelector(".btnAdd--style--js");
+const btnRemove = document.querySelector(".btnRemove--style--js");
+const demand = document.querySelector(".statistic__text--js");
 
-const add = function () {
-  console.log("HELLO 🚀");
+let glassesNeed = 8;
+const glassesSaved = localStorage.getItem("glassesDrinked");
+let glassesDrinked = 8;
+let glassesDemand = glassesNeed - glassesDrinked;
+
+if (glassesSaved) {
+  if (glassesDemand <= glassesSaved) {
+    glassesDrinked = glassesSaved;
+    demand.innerHTML = `${glassesDrinked}`;
+  } else {
+    glassesDrinked = glassesNeed;
+    demand.innerHTML = `${glassesDemand}`;
+  }
+} else {
+  glassesDrinked = 0;
+  demand.innerHTML = `${glassesDemand}`;
+}
+const addGlass = function () {
+  glassesDrinked++;
+  localStorage.setItem("glassesDrinked", glassesDrinked);
+  demand.innerHTML = `${glassesNeed - glassesDrinked}`;
 };
 
-const btnAdd = document.querySelector(".btnAdd--style--js");
-btnAdd.addEventListener("click", add);
-const btnRemove = document.querySelector(".btnRemove--style--js");
-btnRemove.addEventListener("click", add);
+// const checkRunning = document.querySelector(".list__active--running--js");
+// if (checkRunning) {
+//   document.documentElement.style.setProperty("--drops", "none");
+// }
+const removeGlass = function () {
+  glassesDrinked--;
+  localStorage.setItem("glassesDrinked", glassesDrinked);
+  demand.innerHTML = `${glassesNeed - glassesDrinked}`;
+};
+
+btnAdd.addEventListener("click", addGlass);
+btnRemove.addEventListener("click", removeGlass);
