6c2080885191e4a3b4ff22b5847d70ea1e058c9c
Wojciech Sadomski <wsadomski@gmail.com>
2020-06-25 10:54:22 +0200

    first version of aplication

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+![Grafika aplikacji](public/image.png)
+
 # Aplikacja nawadniająca
 
 Apka która pomoże w odpowiednim nawodnieniu organizmu
@@ -6,5 +8,15 @@ Apka która pomoże w odpowiednim nawodnieniu organizmu
 
 1. Stworzenie projektu za pomocą programu Figma
 2. Utworzenie nowego projetku
-3. Code..
-4. Zwiększanie ilości funkcji.
+3. Przeniesienie grafiki za na jezyk HTML
+4. Popranie wszystkich elementów i przypisanie im odpowiednich funkcji.
+5. Wykorzystanie pamięci lokalnej do dziennego zapisu ilości.
+6. Rozszerzenie programu o akywności fizyczne zmieniające zapotrzebowanie na wodę
+7. Wprowadzenie animacji pojawiającej się po przkroczeniu dziennego zapotrzebowania
+8. Niespodzianka dla ludzi zainteresowanych ile należy wypić wody intensywnie "kodując"
+9. Aktualicacja sekcji head i utworzenie grafiki w podglądzie aplikacji.
+
+## Kolejne etapy
+
+1. Zwiększanie ilości zapisanych informacji i wykorzystanie ich do optymalizacji aplikacji.
+2. Kodowanie...
--- src/js/index.js
+++ src/js/index.js
@@ -9,13 +9,22 @@ const btnAdd = document.querySelector(".btnAdd--style--js");
 const btnRemove = document.querySelector(".btnRemove--style--js");
 const demand = document.querySelector(".statistic__text--js");
 const day = new Date().toISOString().slice(0, 10);
+//lista aktywności
+const checkRunning = document.querySelector(".list__active--running--js");
+const checkWalking = document.querySelector(".list__active--walking--js");
+const checkCycling = document.querySelector(".list__active--cycling--js");
+const checkSwimming = document.querySelector(".list__active--swimming-js");
+const checkCodeing = document.querySelector(".list__active--codeing--js");
+//lista zmiennych
 let glassesDrinked = localStorage.getItem(day);
 let glassesNeed = 8;
-console.log(day);
-
+//warunek początkowy
 if (localStorage.getItem(day)) {
   glassesDrinked = localStorage.getItem(day);
   demand.innerHTML = `${glassesDrinked}`;
+  if (glassesDrinked > glassesNeed) {
+    document.documentElement.style.setProperty("--drops", "url(img/drops.svg)");
+  }
 } else {
   localStorage.setItem(day, 0);
   glassesDrinked = localStorage.getItem(day);
@@ -27,24 +36,10 @@ const addGlass = function () {
   demand.innerHTML = `${glassesDrinked}`;
   if (glassesDrinked > glassesNeed) {
     document.documentElement.style.setProperty("--drops", "url(img/drops.svg)");
+    document.documentElement.style.setProperty("--colapse", "2s");
     console.log("działa");
   }
 };
-
-const checkRunning = document.querySelector(".list__active--running--js");
-
-checkRunning.addEventListener("change", function () {
-  if (checkCodeing == "true") {
-    glassesNeed = glassesNeed + 3;
-    console.log(`potrzebujesz ${glassesNeed}`);
-  }
-});
-const checkCodeing = document.querySelector(".list__active--codeing--js");
-
-checkCodeing.addEventListener("change", function () {
-  document.documentElement.style.setProperty("--drops", "url(img/nuke.svg)");
-});
-
 const removeGlass = function () {
   if (glassesDrinked <= 0) {
     glassesDrinked = 0;
@@ -53,10 +48,28 @@ const removeGlass = function () {
     localStorage.setItem(day, glassesDrinked);
     demand.innerHTML = `${glassesDrinked}`;
   }
-  if (glassesDrinked < glassesNeed) {
+  if (glassesDrinked <= glassesNeed) {
     document.documentElement.style.setProperty("--drops", "none");
+    document.documentElement.style.setProperty("--colapse", "0s");
   }
 };
-
+//checkbox
+checkRunning.addEventListener("change", function () {
+  glassesNeed = glassesNeed + 3;
+  console.log(`potrzebujesz ${glassesNeed}`);
+});
+checkWalking.addEventListener("change", function () {
+  glassesNeed = glassesNeed + 2;
+  console.log(`potrzebujesz ${glassesNeed}`);
+});
+checkCycling.addEventListener("change", function () {
+  glassesNeed = glassesNeed + 4;
+  console.log(`potrzebujesz ${glassesNeed}`);
+});
+checkSwimming.addEventListener("change", function () {
+  glassesNeed = glassesNeed + 3;
+  console.log(`potrzebujesz ${glassesNeed}`);
+});
+//przyciski
 btnAdd.addEventListener("click", addGlass);
 btnRemove.addEventListener("click", removeGlass);
